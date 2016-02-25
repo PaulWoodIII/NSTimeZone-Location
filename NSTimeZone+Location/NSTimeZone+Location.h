18644e04eaee649b83ec6154c3cb40a7fe1f526a
Paul Wood III <paul.wood.iii@gmail.com>
2016-02-24 19:27:28 -0500

    init

---
+++ NSTimeZone+Location/NSTimeZone+Location.h
@@ -0,0 +1,19 @@
+//
+//  NSTimeZone+Location.h
+//  NSTimeZone+Location
+//
+//  Created by Paul Wood on 2/24/16.
+//  Copyright © 2016 Paul Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSTimeZone+Location.
+FOUNDATION_EXPORT double NSTimeZone_LocationVersionNumber;
+
+//! Project version string for NSTimeZone+Location.
+FOUNDATION_EXPORT const unsigned char NSTimeZone_LocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSTimeZone_Location/PublicHeader.h>
+
+
---
+++ NSTimeZone+LocationTests/NSTimeZone_LocationTests.swift
@@ -0,0 +1,31 @@
+//
+//  NSTimeZone_LocationTests.swift
+//  NSTimeZone+LocationTests
+//
+//  Created by Paul Wood on 2/24/16.
+//  Copyright © 2016 Paul Wood. All rights reserved.
+//
+
+import XCTest
+@testable import NSTimeZone_Location
+
+class NSTimeZone_LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testNewYork() {
+        let tz = NSTimeZone.closestTimeZoneWithLocation(CLLocation(latitude: 40.7127,
+            longitude: -74.0059))
+        print(tz)
+        XCTAssertEqual(NSTimeZone(abbreviation: "EST"), tz)
+    }
+    
+}
